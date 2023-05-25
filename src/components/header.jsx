2537e6c09c49f95f613ace3235f5d88573a355ef
Kristine Christensen <christensen@morainevalley.edu>
2023-05-24 23:29:17 -0500

    last updates, I promise

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,14 @@
 import { Link } from 'react-router-dom'
+import kcLogo from '../assets/thickswKCLogo.png'
 
 export default function Header() {
   return (
-    <header>
-      <Link to="/">Home</Link>
-      <Link to="/about">About Me</Link>
-      <Link to="/contact">Contact</Link>
+
+  <header>
+    <Link to="/"><img src={kcLogo} style={{height:'40px', margin:'5px 0 5px 25px'}} /></Link>
+    <Link to="/">Home</Link>
+    <Link to="/about">About Me</Link>
+    <Link to="/contact">Contact</Link>
     </header>
   )
 }
