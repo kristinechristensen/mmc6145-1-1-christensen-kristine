775c6c1dba1e5b2e902afbc92ce7dfb7771967d9
Kristine Christensen <christensen@morainevalley.edu>
2023-05-24 23:29:51 -0500

    logo

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
 
   <header>
-    <Link to="/"><img src={kcLogo} style={{height:'40px', margin:'5px 0 5px 25px'}} /></Link>
+    <Link to="/"><img src={kcLogo} style={{height:'30px', margin:'0 0 0 25px'}} /></Link>
     <Link to="/">Home</Link>
     <Link to="/about">About Me</Link>
     <Link to="/contact">Contact</Link>
