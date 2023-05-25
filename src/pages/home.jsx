1e09b10c0e3d107e16685832074f4500e47c7705
Kristine Christensen <christensen@morainevalley.edu>
2023-05-24 23:09:18 -0500

    quickTouchUp

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,10 @@
 import styles from '../styles/home.module.css'
 import Project from '../components/project'
 
-import art1 from '../assets/DragonScales.jpg'
+import art1 from '../assets/medusaTable.jpg'
 import art2 from '../assets/hands.jpg'
 import art3 from '../assets/recoil.jpg'
-import art4 from '../assets/medusaTable.jpg'
+import art4 from '../assets/DragonScales.jpg'
 import art5 from '../assets/spiderBookEnds.jpg'
 import art6 from '../assets/graveyard.jpg'
 
@@ -20,9 +20,8 @@ export default function Home() {
       <h1 className={styles.heading}>Welcome to My Home Page</h1>
       <h2> So Glad You're Here!</h2>
       <div className={styles.container}>
-      {artWork.map((art, i) => <Project key={i}><img src={art} /></Project>)}
+          {artWork.map((art, i) => <Project key={i}><img src={art} /></Project>)}
       </div>
-    
       
     </main>
     
